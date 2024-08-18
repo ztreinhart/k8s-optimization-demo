00371500f8c3522da338691679292001b91d03fb
Zach Reinhart <ztreinhart@gmail.com>
2024-08-18 00:49:36 -0500

    Adding some data models

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import models
 
 # Set up command line arguments
 arg_parser = argparse.ArgumentParser()
@@ -7,33 +8,40 @@ arg_parser.add_argument('input_file', help='Path to JSON input file')
 args = arg_parser.parse_args()
 
 # Dictionary for our parsed data
-data = dict()
+data = {}
 
 # Open the file and load its contents as JSON
 try:
     with open(args.input_file, 'r') as file:
-        data = json.load(file)
+        data = json.load(file, object_hook=models.podNodeDecoder)
 
 except FileNotFoundError:
     print(f"Error: File '{args.input_file}' not found.")
+    exit(1)
 except json.JSONDecodeError:
     print(f"Error: File '{args.input_file}' contains invalid JSON.")
+    exit(1)
 except Exception as e:
     print(f"An error occurred: {str(e)}")
+    exit(1)
+
+# Parsed data
+pods = data['pods']
+nodes = data['nodes']
 
 # Summarize pod data
-print(f"Number of pods: {len(data['pods'])}\n")
+print(f"Number of pods: {len(pods)}\n")
 
 total_CPU_reqs = 0
 total_CPU_limits = 0
 total_mem_reqs = 0
 total_mem_limits = 0
 
-for pod in data['pods']:
-    total_CPU_reqs += pod['cpu_request']
-    total_CPU_limits += pod['cpu_limit']
-    total_mem_reqs += pod['memory_request']
-    total_mem_limits += pod['memory_limit']
+for pod in pods:
+    total_CPU_reqs += pod.cpu_request
+    total_CPU_limits += pod.cpu_limit
+    total_mem_reqs += pod.memory_request
+    total_mem_limits += pod.memory_limit
 
 print(f"Total CPU requests: {total_CPU_reqs}")
 print(f"Total CPU limits: {total_CPU_limits}\n")
---
+++ models.py
@@ -0,0 +1,28 @@
+pod_keys = ["name", "namespace", "cpu_request", "cpu_limit", "memory_request", "memory_limit"]
+node_keys = ["name", "cpu_capacity", "memory_capacity"]
+
+class Pod:
+    def __init__(self, name="", namespace="", cpu_request=0, cpu_limit=0, memory_request=0, memory_limit=0):
+        self.name = name
+        self.namespace = namespace
+        self.cpu_request = cpu_request
+        self.cpu_limit = cpu_limit
+        self.memory_request = memory_request
+        self.memory_limit = memory_limit
+
+class Node:
+    def __init__(self, name="", cpu_capacity=0, memory_capacity=0):
+        self.name = name
+        self.cpu_capacity = cpu_capacity
+        self.memory_capacity = memory_capacity
+
+def podNodeDecoder(obj):
+    if type(obj) == dict:
+        keys_set = set(obj.keys())
+        if keys_set == set(pod_keys):
+            return Pod(obj['name'], obj['namespace'], obj['cpu_request'], obj['cpu_limit'], obj['memory_request'],obj['memory_limit'])
+        elif keys_set == set(node_keys):
+            return Node(obj['name'], obj['cpu_capacity'], obj['memory_capacity'])
+        else:
+            return obj
+    return obj
