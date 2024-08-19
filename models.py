11e239d46c661ee5cca21c29adbde19e40e9bded
Zach Reinhart <ztreinhart@gmail.com>
2024-08-18 22:35:29 -0500

    Adding some comments and removing a redundant return in models.py

--- models.py
+++ models.py
@@ -1,6 +1,9 @@
+# pod_keys is the list of keys used in the JSON input file to describe a pod
 pod_keys = ["name", "namespace", "cpu_request", "cpu_limit", "memory_request", "memory_limit"]
+# node_keys is the list of keys used in the JSON input file to describe a node
 node_keys = ["name", "cpu_capacity", "memory_capacity"]
 
+# The Pod class describes a Kubernetes pod
 class Pod:
     def __init__(self, name="", namespace="", cpu_request=0, cpu_limit=0, memory_request=0, memory_limit=0):
         self.name = name
@@ -10,12 +13,15 @@ class Pod:
         self.memory_request = memory_request
         self.memory_limit = memory_limit
 
+# The Node class describes a Kubernetes node
 class Node:
     def __init__(self, name="", cpu_capacity=0, memory_capacity=0):
         self.name = name
         self.cpu_capacity = cpu_capacity
         self.memory_capacity = memory_capacity
 
+# pod_node_decoder is a decoder that can be used with the json package to parse JSON descriptions of pods and nodes
+# into Pod and Node class instances.
 def pod_node_decoder(obj):
     if type(obj) == dict:
         keys_set = set(obj.keys())
@@ -23,6 +29,4 @@ def pod_node_decoder(obj):
             return Pod(obj['name'], obj['namespace'], obj['cpu_request'], obj['cpu_limit'], obj['memory_request'],obj['memory_limit'])
         elif keys_set == set(node_keys):
             return Node(obj['name'], obj['cpu_capacity'], obj['memory_capacity'])
-        else:
-            return obj
     return obj
